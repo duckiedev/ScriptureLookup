0e390963f2b591635986e2a68158e3e30d078481
duckie <jshirley1990@gmail.com>
2026-08-06 15:44:07 -0500

    Final commit
    Functioning when called.

--- obj/Debug/net10.0/ScriptureLookup.AssemblyInfo.cs
+++ obj/Debug/net10.0/ScriptureLookup.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScriptureLookup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34d9a4f3f0bc703c0a88cedf197cb5a9d59abcf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScriptureLookup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScriptureLookup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
